fa1bf712a9c9d0c78e8af51484aa0c91639ec1f7
Kevin <Kesalvador@s.sfusd.edu>
2023-02-06 14:18:36 -0800

    Destroying projectiles step 6

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -8,7 +8,7 @@ public class PlayerControler : MonoBehaviour
     public float speed=10.0f;
     public float xRange=10.0f;
 
-    public GameObject projectilePrefabs;
+    public GameObject projectilePrefab;
     
     // Start is called before the first frame update
     void Start()
@@ -31,9 +31,11 @@ public class PlayerControler : MonoBehaviour
         horizontalInput=Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
         if (Input.GetKeyDown(KeyCode.Space))
-        (
-          //Launch a projectile from the player
-        )
+        {
+          Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+          
+        
     }
 
 }
